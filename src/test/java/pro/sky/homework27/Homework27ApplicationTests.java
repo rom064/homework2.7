81d1ca7ccaa523b6d57f01597aaf8024787244a3
rom064 <rom064@mail.ru>
2023-05-09 23:31:52 +0500

    ДЗ 2.7

---
+++ src/test/java/pro/sky/homework27/Homework27ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.homework27;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework27ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
